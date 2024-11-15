f0512c2d18134675a4269818daa245ed8d068e3d
Mustafa-Aba <mustafa_aba@outlook.com>
2024-11-15 03:18:09 +0300

    updated

--- cypress/e2e/petstoreApiTest/4-deleteMethod.cy.js
+++ cypress/e2e/petstoreApiTest/4-deleteMethod.cy.js
@@ -1,19 +1,19 @@
 describe("DELETE METHOD", () => {
-    const myPet = {
-      id: 956232515615,
-      name: "karabas",
-      status: "available",
-      updatedStatus: "sold",
-    };
-  
-    it("Pet ID ile silme işlemi (Delete)", () => {
-      cy.request("DELETE", `/pet/${myPet.id}`).then((response) => {
-        expect(response.status).to.equal(200);
-      });
-      cy.request({ url: `/pet/${myPet.id}`, failOnStatusCode: false }).then(
-        (response) => {
-          expect(response.status).to.equal(404);
-        }
-      );
+  const myPet = {
+    id: 956232515615,
+    name: "karabas",
+    status: "available",
+    updatedStatus: "sold",
+  };
+
+  it("Pet ID ile silme işlemi (Delete)", () => {
+    cy.request("DELETE", `/pet/${myPet.id}`).then((response) => {
+      expect(response.status).to.equal(200);
     });
+    cy.request({ url: `/pet/${myPet.id}`, failOnStatusCode: false }).then(
+      (response) => {
+        expect(response.status).to.equal(404);
+      }
+    );
   });
+});
